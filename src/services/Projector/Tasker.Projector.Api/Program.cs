4b110e7061dc9c61f8988d743f99c5c8e4ed6611
alexomur <alexomur111@gmail.com>
2025-11-02 14:03:06 +0300

    fix: warning on start (remove local https)

--- src/services/Projector/Tasker.Projector.Api/Program.cs
+++ src/services/Projector/Tasker.Projector.Api/Program.cs
@@ -36,7 +36,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+if (app.Environment.IsProduction())
+{
+    app.UseHttpsRedirection();
+}
 app.MapPrometheusScrapingEndpoint();
 
 app.MapHealthChecks("/healthz", new HealthCheckOptions {
