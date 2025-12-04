2035fc8878663bdc2bf0be6bc35696944f1da277
alexomur <alexomur111@gmail.com>
2025-12-04 17:17:00 +0300

    chore: assignee migration

---
+++ src/services/BoardWrite/Tasker.BoardWrite.Infrastructure/Migrations/20251204140151_AddCardAssignees.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tasker.BoardWrite.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCardAssignees : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "assignee_user_ids",
+                table: "cards",
+                type: "longtext",
+                nullable: false,
+                collation: "utf8mb4_0900_ai_ci")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "assignee_user_ids",
+                table: "cards");
+        }
+    }
+}
